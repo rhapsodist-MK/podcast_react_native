f190709e31837537ccc030e28b353a83e86e1d38
김민우 <rhapsodist@gimmin-uui-MacBookPro.local>
2020-03-30 03:54:43 +0900

    add vector icons && add tab navigation icon and color

--- src/navigation/TabNavigation.js
+++ src/navigation/TabNavigation.js
@@ -1,23 +1,39 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import FeatherIcon from 'react-native-vector-icons/Feather'
 
 import {routes} from './router'
+import { metrics } from '@constants/metrics'
+import { theme } from '@constants/theme'
 
 import HomeScreen from '@components/home/HomeScreen'
 import DownloadsScreen from '@components/downloads/DownloadsScreen'
-import LibraryScreen from '@components/library/LibraryScreen'
+import LibraryScreen from '@components/library/LibraryScreen' 
 import ProfileScreen from '@components/profile/ProfileScreen'
+
 
 
 const Tab = createBottomTabNavigator()
 
 const TabNavigation = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name={routes.HOME} component={HomeScreen}/>
-      <Tab.Screen name={routes.LIBRARY} component={LibraryScreen}/>
-      <Tab.Screen name={routes.DOWNLOADS} component={DownloadsScreen}/>
-      <Tab.Screen name={routes.PROFILE} component={ProfileScreen}/>
+    <Tab.Navigator tabBarOptions={{
+      activeTintColor: theme.color.blueDark,
+      inactiveTintColor: theme.color.grayLight,
+      showLabel: false
+    }}>
+      <Tab.Screen options={{
+        tabBarIcon: ({color}) => <FeatherIcon name="home" size={metrics.tabIconSize} color={color}/>
+      }} name={routes.HOME} component={HomeScreen}/>
+      <Tab.Screen options={{
+        tabBarIcon: ({color}) => <FeatherIcon name="inbox" size={metrics.tabIconSize} color={color}/>
+      }} name={routes.LIBRARY} component={LibraryScreen}/>
+      <Tab.Screen options={{
+        tabBarIcon: ({color}) => <FeatherIcon name="headphones" size={metrics.tabIconSize} color={color}/>
+      }} name={routes.DOWNLOADS} component={DownloadsScreen}/>
+      <Tab.Screen options={{
+        tabBarIcon: ({color}) => <FeatherIcon name="user" size={metrics.tabIconSize} color={color}/>
+      }} name={routes.PROFILE} component={ProfileScreen}/>
     </Tab.Navigator>
   )
 }
